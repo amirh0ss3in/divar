a3d71a3594550816936442f9add5dd829ce49e9c
Amirhossein Rezaei <rezaeeamirhosein@gmail.com>
2023-10-20 13:27:41 +0330

    dp

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,31 +1,35 @@
 import json
 import os
+import logging
+from json.decoder import JSONDecodeError
+
 
 def extract_data(file_path):
-    # Load the data from the JSON file
+    """Extracts the desired data from the JSON file."""
+
     with open(file_path, encoding='utf-8') as f:
         info = json.load(f)
-    
-    # Extract the desired data from the JSON structure
+
     data = {
-        "Date": info.get("widgets").get('header').get('date'),
+        "Date": info.get("widgets").get("header").get("date"),
         "RENT": info.get("data").get("webengage").get("rent"),
         "CREDIT": info.get("data").get("webengage").get("credit"),
         "CITY": info.get("data").get("webengage").get("city"),
-        "Meterage": info.get("widgets").get('list_data')[0].get("items")[0].get('value'),
-        "Construction": info.get("widgets").get('list_data')[0].get("items")[1].get('value'),
-        "ROOMS": info.get("widgets").get('list_data')[0].get("items")[2].get('value'),
-        "floor": info.get("widgets").get('list_data')[6].get("value"),
-        "Elevator": info.get("widgets").get('list_data')[7].get("items")[0].get("available"),
-        "Parking": info.get("widgets").get('list_data')[7].get("items")[1].get("available"),
-        "Warehouse": info.get("widgets").get('list_data')[7].get("items")[2].get("available"),
+        "Meterage": info.get("widgets").get("list_data")[0].get("items")[0].get("value"),
+        "Construction": info.get("widgets").get("list_data")[0].get("items")[1].get("value"),
+        "ROOMS": info.get("widgets").get("list_data")[0].get("items")[2].get("value"),
+        "floor": info.get("widgets").get("list_data")[6].get("value"),
+        "Elevator": info.get("widgets").get("list_data")[7].get("items")[0].get("available"),
+        "Parking": info.get("widgets").get("list_data")[7].get("items")[1].get("available"),
+        "Warehouse": info.get("widgets").get("list_data")[7].get("items")[2].get("available"),
         "location": info.get("data").get("lacation")
     }
-    
+
     return data
 
 def create_new_dict(data):
-    # Create a new dictionary with the desired key-value pairs
+    """Creates a new dictionary with the desired key-value pairs."""
+
     new_dict = {
         "CITY": data["CITY"],
         "Date": data["Date"],
@@ -39,19 +43,35 @@ def create_new_dict(data):
         "floor": data["floor"],
         "location": data["location"]
     }
-    
+
     return new_dict
 
+def main():
+    """Main function."""
 
-# Example usage
-files_path = os.listdir("Results/apartment-rent/2/")
+    files_path = "Results/apartment-rent/1/"
+    files_names = os.listdir(files_path)
+    logging.basicConfig(filename="errors.log", level=logging.INFO)
 
-print(files_path)
+    success = 0 
+    for file_name in files_names:
+        try:
+            data = extract_data(files_path + file_name)
+            new_dict = create_new_dict(data)
+            # print(new_dict)
+            success += 1
+        except FileNotFoundError:
+            logging.error(f"File not found: {file_name}")
+            print(f"File not found: {file_name}")
+        except JSONDecodeError:
+            logging.error(f"Invalid JSON file: {file_name}")
+            print(f"Invalid JSON file: {file_name}")
+        except:
+            logging.error(f"Unexpected error: {file_name}")
+            print(f"Unexpected error: {file_name}")
+    fail = len(files_names) - success
+    print("failed:",fail)
+    print("success:",success)
 
-# Extract the data from the JSON file
-data = extract_data(file_path)
-
-# Create a new dictionary with the selected key-value pairs
-new_dict = create_new_dict(data)
-
-# Print the new diction
+if __name__ == "__main__":
+    main()
