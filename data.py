332eba6b3ce684612f43589bbd8ec1ee48154107
Amirhossein Rezaei <rezaeeamirhosein@gmail.com>
2023-12-11 23:24:40 +0330

    data visualization

--- data.py
+++ data.py
@@ -3,35 +3,35 @@ import matplotlib.pyplot as plt
 import json
 
 def process_data(info):
-    data = pd.DataFrame.from_dict(info, orient='index', columns=['CREDIT', 'RENT', 'longitude', 'latitude','Meterage','District', "Construction","Parking","Elevator","Warehouse"])
+    data = pd.DataFrame.from_dict(info, orient='index', columns=['PRICE', 'longitude', 'latitude','Meterage','District', "Construction","Parking","Elevator","Warehouse"])
 
     districts = set(data['District'])
 
-    data['CREDIT'] /= 1_000_000
-    data['RENT'] /= 1_000_000
+    data['PRICE'] /= 1_000_000
     data['longitude'], data['latitude'] = data['longitude'].astype(float), data['latitude'].astype(float)
-    mask_prices = (data['CREDIT'] < 100) | (data['CREDIT'] > 2000) | (data['RENT'] < 1) | (data['RENT'] > 100)
-    data = data.loc[~mask_prices].dropna(subset=['CREDIT', 'RENT'])
+    # mask_prices = (data['PRICE'] < 100) | (data['PRICE'] > 2000)
+    # data = data.loc[~mask_prices].dropna(subset=['PRICE'])
 
-    locs = data[['longitude', 'latitude']].dropna()
-    mask_locs = (locs['latitude'] > 36) | (locs['latitude'] < 35.55) | (locs['longitude'] < 51.10) | (locs['longitude'] > 51.60)
-    data_location = data.loc[~(mask_locs|mask_prices)].dropna()
-    return data, data_location, districts
 
-files_path = "Results/apartment-rent/1/"
+    return data, districts
+
+files_path = "Results/apartment-sell/1/"
  
 with open(files_path+"table.json" , encoding='utf-8') as f:
     info = json.load(f)
 
+
 print("Data read complete.")
-data, data_location, districts = process_data(info=info)
+data, districts = process_data(info=info)
 print("Data process complete.")
 
-data_all_true = data[(data["Parking"] == True) & (data["Elevator"] == True) & (data["Warehouse"] == True)]
+# data_all_true = data[(data["Parking"] == True) & (data["Elevator"] == True) & (data["Warehouse"] == True)]
+data_all_true = data
 
-data_all_true_poonak = data_all_true[data_all_true["District"] == "پونک"]
-print(data_all_true_poonak)
+print(data_all_true["District"].describe())
+
+data_all_true_poonak = data_all_true[data_all_true["District"] == "سعادت‌آباد"]
 
 
-plt.scatter(data_all_true_poonak['Meterage'],data_all_true_poonak['CREDIT'])
+plt.scatter(data_all_true_poonak['Meterage'], data_all_true_poonak['PRICE'])
 plt.show()
